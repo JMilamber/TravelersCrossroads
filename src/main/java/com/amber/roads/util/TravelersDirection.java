986fca64990b4d67eebcf14fd01c6fdf01f2ee78
JMilamber <socky79.turbulance@gmail.com>
2025-07-02 01:14:37 -0400

    Change TravelersDirection to contain tertiary directions

--- src/main/java/com/amber/roads/util/TravelersDirection.java
+++ src/main/java/com/amber/roads/util/TravelersDirection.java
@@ -1,33 +1,45 @@
 package com.amber.roads.util;
 
+import com.amber.roads.world.PathPos;
+import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.StringRepresentable;
-import net.minecraft.world.level.ChunkPos;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static com.amber.roads.util.TravelersUtil.roundToHalf;
+import static java.lang.Math.abs;
+
 public enum TravelersDirection implements StringRepresentable {
-    NORTH(0, 0, -1, "north"),
-    NORTHEAST(1, 1, -1, "northeast"),
-    EAST(2, 1, 0, "east"),
-    SOUTHEAST(3, 1, 1, "southeast"),
-    SOUTH(4, 0, 1, "south"),
-    SOUTHWEST(5, -1, 1, "southwest"),
-    WEST(6, -1, 0, "west"),
-    NORTHWEST(7, -1, -1, "northwest");
+    N(0, 0f, -1f, "N"),
+    NNE(1, 0.5f, -1f, "NNE"),
+    NE(2, 1f, -1f, "NE"),
+    ENE(3, 1f, -0.5f, "ENE"),
+    E(4, 1f, 0f, "E"),
+    ESE(5, 1f, 0.5f, "ESE"),
+    SE(6, 1f, 1f, "SE"),
+    SSE(7, 0.5f, 1f, "SSE"),
+    S(8, 0f, 1f, "S"),
+    SSW(9, -0.5f, 1f, "SSW"),
+    SW(10, -1f, 1f, "SW"),
+    WSW(11, -1f, 0.5f, "WSW"),
+    W(12, -1f, 0f, "W"),
+    WNW(13, -1f, -0.5f, "WNW"),
+    NW(14, -1f, -1f, "NW"),
+    NNW(15, -0.5f, -1f, "NNW");
 
     private final int index;
-    private final int x;
-    private final int z;
+    private final float x;
+    private final float z;
     private final String name;
-    private static final int[][] indexByXZ = {{7, 6, 5}, {0, -1, 4}, {1, 2, 3}};
+    private static final int[][] indexByXZ = {{14, 13, 12, 11, 10}, {15, -1, -1, -1, 9}, {0, -1, -1, -1, 8}, {1, -1, -1, -1, 7}, {2, 3, 4, 5, 6}};
     private static final TravelersDirection[] VALUES = values();
 
     TravelersDirection(
-            int index, int x, int z, String name
+            int index, float x, float z, String name
     ) {
         this.index = index;
         this.x = x;
@@ -40,41 +52,100 @@ public enum TravelersDirection implements StringRepresentable {
     }
 
     public int getOppositeIndex() {
-        return index > 3 ? index - 4 : index + 4;
+        return index > 7 ? index - 8 : index + 8;
     }
 
     public int getModifiedIndex(int modifier) {
         int newIndex = index + modifier;
 
-        if (newIndex > 7) {
-            newIndex = newIndex - 8;
+        if (newIndex > 15) {
+            newIndex = newIndex - 16;
         } else if (newIndex < 0) {
-            newIndex = newIndex + 8;
+            newIndex = newIndex + 16;
         }
         // TravelersCrossroads.LOGGER.debug("Initial index {}  new index {}", index, newIndex);
         return newIndex;
     }
 
+
     public Collection<? extends TravelersDirection> getNeighbors() {
-        return List.of(this, VALUES[getModifiedIndex(1)], VALUES[getModifiedIndex(-1)]);
+        return List.of(this,
+                VALUES[getModifiedIndex(1)], VALUES[getModifiedIndex(-1)],
+                VALUES[getModifiedIndex(2)], VALUES[getModifiedIndex(-2)]
+        );
+    }
+
+    public boolean isN() {
+        return this.z < 0;
+    }
+
+    public boolean isE() {
+        return this.x > 0;
+    }
+
+    public boolean isS() {
+        return this.z > 0;
+    }
+
+    public boolean isW() {
+        return this.x < 0;
+    }
+
+    public boolean isVertical() {
+        return this.x == 0;
     }
 
     public TravelersDirection getOpposite() {
         return VALUES[getOppositeIndex()];
     }
 
-    public int getX() {
+    public float getX() {
         return x;
     }
 
-    public int getZ() {
+    public int nextPosX(int distance) {
+        return (int) (x * distance);
+    }
+
+    public float nextPosXf(int distance) {
+        return x * distance;
+    }
+
+    public int nextSectionCenterX(int distance) {
+        return nextPosX(distance) / 2;
+    }
+
+    public float getZ() {
         return z;
     }
 
-    public static TravelersDirection directionFromChunks(ChunkPos pos1, ChunkPos pos2) {
-        int x = Mth.clamp(pos2.x - pos1.x, -1, 1);
-        int z = Mth.clamp(pos2.z - pos1.z, -1, 1);
-        return VALUES[indexByXZ[x + 1][z + 1]];
+    public int nextPosZ(int distance) {
+        return (int) (z * distance);
+    }
+
+    public float nextPosZf(int distance) {
+        return z * distance;
+    }
+
+    public int nextSectionCenterZ(int distance) {
+        return nextPosZ(distance) / 2;
+    }
+
+    public PathPos nextPos(PathPos pos, int distance) {
+        return new PathPos(pos.x + nextPosX(distance), pos.z + nextPosZ(distance));
+    }
+
+    public BlockPos nextSectionCenter(PathPos pos, int distance) {
+        return new BlockPos(pos.x + nextSectionCenterX(distance), 0, pos.z + nextSectionCenterZ(distance));
+    }
+
+
+    public static TravelersDirection directionFromPos(PathPos pos1, PathPos pos2, float distance) {
+        float max = Math.max(Math.max(abs(pos2.x - pos1.x), abs(pos2.z - pos1.z)), distance);
+        double x = Mth.clamp(roundToHalf((pos2.x - pos1.x) / max), -1f, 1f);
+        double z = Mth.clamp(roundToHalf((pos2.z - pos1.z) / max), -1f, 1f);
+        // TravelersCrossroads.LOGGER.debug("Direction x {} z {} indexX {} indexZ {}", x, z, (x + 1) * 2, (z + 1) * 2);
+        return VALUES[indexByXZ[(int) ((x + 1) * 2)][(int) ((z + 1) * 2)]];
     }
 
     public static ArrayList<TravelersDirection> weightedList(TravelersDirection direction) {
@@ -83,10 +154,6 @@ public enum TravelersDirection implements StringRepresentable {
         int above2 = direction.getModifiedIndex(2);
         int below = direction.getModifiedIndex(-1);
         int below2 = direction.getModifiedIndex(-2);
-
-        list.add(direction);
-        list.add(direction);
-        list.add(direction);
 
         list.add(VALUES[above]);
         list.add(VALUES[above]);
@@ -105,9 +172,7 @@ public enum TravelersDirection implements StringRepresentable {
 
         int above = direction.getModifiedIndex(1);
         int below = direction.getModifiedIndex(-1);
-        list.add(direction);
-        list.add(direction);
-        list.add(direction);
+
         list.add(VALUES[above]);
         list.add(VALUES[above]);
         list.add(VALUES[below]);
