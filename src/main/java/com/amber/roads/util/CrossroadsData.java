facdba5da42a297ae3fab4669c4138eb58b67fe8
JMilamber <socky79.turbulance@gmail.com>
2025-07-15 22:05:55 -0400

    Remove beginnings list

--- src/main/java/com/amber/roads/util/CrossroadsData.java
+++ src/main/java/com/amber/roads/util/CrossroadsData.java
@@ -1,10 +1,9 @@
 package com.amber.roads.util;
 
-import com.amber.roads.world.TravelersCrossroad;
+import com.amber.roads.world.PathNode;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
-import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraft.world.level.storage.DimensionDataStorage;
 
@@ -12,9 +11,8 @@ import java.util.ArrayList;
 
 public class CrossroadsData extends SavedData {
 
-    private ArrayList<TravelersPath> paths = new ArrayList<>();
-    private ArrayList<TravelersCrossroad> crossroads = new ArrayList<>();
-    private ArrayList<ChunkPos> beginnings = new ArrayList<>();
+    private ArrayList<TravelersPath> unfinishedPaths = new ArrayList<>();
+    private ArrayList<PathNode> pathNodes = new ArrayList<>();
     // Create new instance of saved data
     public static CrossroadsData create() {
         return new CrossroadsData();
@@ -29,15 +27,11 @@ public class CrossroadsData extends SavedData {
         CrossroadsData data = CrossroadsData.create();
         int length = tag.getInt("len");
         for (int i = 0; i < length; i++) {
-            data.paths.add(new TravelersPath(tag, i));
+            data.unfinishedPaths.add(new TravelersPath(tag, i));
         }
-        int cross_length = tag.getInt("cross_len");
-        for (int i = 0; i < cross_length; i++) {
-            data.crossroads.add(new TravelersCrossroad(tag, i));
-        }
-        int beginning_length = tag.getInt("begin_len");
-        for (int i = 0; i < beginning_length; i++) {
-            data.beginnings.add(new ChunkPos(NbtUtils.readBlockPos(tag, "begin" + i).orElseThrow()));
+        int nodes_len = tag.getInt("nodes_len");
+        for (int i = 0; i < nodes_len; i++) {
+            data.pathNodes.add(new PathNode(NbtUtils.readBlockPos(tag, "nodes" + i).orElseThrow()));
         }
         // Load saved data
         return data;
@@ -46,57 +40,52 @@ public class CrossroadsData extends SavedData {
     @Override
     public CompoundTag save(CompoundTag tag, HolderLookup.Provider registries) {
 
-        tag.putInt("len", this.paths.size());
-        for (int i = 0; i < this.paths.size(); i++) {
-            TravelersPath path = this.paths.get(i);
+        tag.putInt("len", this.unfinishedPaths.size());
+        for (int i = 0; i < this.unfinishedPaths.size(); i++) {
+            TravelersPath path = this.unfinishedPaths.get(i);
             path.save(tag, i);
         }
 
-        tag.putInt("cross_len", this.crossroads.size());
-        for (int i = 0; i < this.crossroads.size(); i++) {
-            TravelersCrossroad crossroad = this.crossroads.get(i);
-            crossroad.save(tag, i);
-        }
-
-        tag.putInt("begin_len", this.beginnings.size());
-        for (int i = 0; i < this.beginnings.size(); i++) {
-            tag.put("begin" + i, NbtUtils.writeBlockPos(this.beginnings.get(i).getWorldPosition()));
+        tag.putInt("nodes_len", this.pathNodes.size());
+        for (int i = 0; i < this.pathNodes.size(); i++) {
+            tag.put("nodes" + i, NbtUtils.writeBlockPos(this.pathNodes.get(i).asBlockPos()));
         }
         return tag;
     }
 
-    public ArrayList<TravelersPath> getPaths() {
-        return paths;
+    public ArrayList<TravelersPath> getUnfinishedPaths() {
+        return unfinishedPaths;
     }
 
-    public ArrayList<TravelersCrossroad> getCrossroads() {
-        return crossroads;
-    }
 
-    public ArrayList<ChunkPos> getBeginnings() {
-        return beginnings;
+    public ArrayList<PathNode> getPathNodes() {
+        return pathNodes;
     }
 
     public boolean addPath(TravelersPath newPath) {
-        // Change data in saved data
-        for (TravelersPath path : this.paths) {
-            if (path.getEnd().x == newPath.getEnd().x && path.getEnd().z == newPath.getEnd().z) {
-                return false;
-            }
+        if (this.unfinishedPaths.contains(newPath)) {
+            return false;
         }
-        this.paths.add(newPath);
+        // Change data in saved data
+        this.unfinishedPaths.add(newPath);
         // Call set dirty if data changes
         this.setDirty();
         return true;
     }
 
-    public void addCrossroad(TravelersCrossroad crossroad) {
-        this.crossroads.add(crossroad);
+    public void addPathNode(PathNode node) {
+        if (this.pathNodes.contains(node)) {
+            return;
+        }
+
+        this.pathNodes.add(node);
+        // Call set dirty if data changes
         this.setDirty();
     }
 
-    public void addBeginning(ChunkPos pos) {
-        this.beginnings.add(pos);
+    public void removePath(TravelersPath path) {
+        this.unfinishedPaths.remove(path);
+        // Call set dirty if data changes
         this.setDirty();
     }
 }
